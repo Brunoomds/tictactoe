3ef5e5ae872f92206e486c7edde23df8bc772bf3
Brunoomds <brunoo.m.d.silva@gmail.com>
2023-04-02 15:37:57 -0300

    wip

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
 				<div class="bg-slate-800 py-2 px-4 rounded font-semibold">
 					<div v-if="isRoundOver" class="flex justify-center items-center gap-2 animate-pulse-fast">
 						<component :is="slots[winnerSlots[0]] || IconMarkX" class="w-4" />
-						<p v-text="winnerSlots.length ? 'WINS' : 'TIE'" />
+						<p v-text="winnerSlots.length ? 'WINS' : 'TIE'" class="text-white" />
 						<component :is="slots[winnerSlots[0]] || IconMarkO" class="w-4" />
 					</div>
 
@@ -84,7 +84,7 @@ import IconGithub from "@/components/icons/IconGithub.vue";
 import { computed } from "@vue/reactivity";
 
 const slots = ref(Array(9).fill(null));
-const xTurn = ref(false);
+const xTurn = ref(true);
 const toggleFirstMove = ref(true);
 const isRoundOver = ref(false);
 const botMode = ref(true);
@@ -141,11 +141,49 @@ function clearBoard() {
 }
 
 function handleBotPlay() {
-	const emptySlots = slots.value.reduce((acc, slot, index) => (slot === null ? [...acc, index] : acc), []);
-	const randomIndex = Math.floor(Math.random() * emptySlots.length);
-	const randomSlot = emptySlots[randomIndex];
+	const avaiableSlots = slots.value.reduce((acc, slot, index) => (slot === null ? [...acc, index] : acc), []);
+	const randomMove = avaiableSlots[Math.floor(Math.random() * avaiableSlots.length)];
+	const bestMove = minimax(slots.value, xTurn.value).index;
 
-	setTimeout(() => handlePlay(randomSlot), 500);
+	const randomChance = Math.random() < 0.1;
+	if (randomChance) console.log("random move");
+
+	setTimeout(() => handlePlay(randomChance ? randomMove : bestMove), 0);
+}
+
+function minimax(board, isMaximizing, alpha = -Infinity, beta = Infinity) {
+	const availableSlots = board.reduce((acc, slot, index) => (slot === null ? [...acc, index] : acc), []);
+
+	if (checkWinner(board, xTurn.value)) return { score: -10 };
+	if (checkWinner(board, !xTurn.value)) return { score: 10 };
+	if (availableSlots.length === 0) return { score: 0 };
+
+	let bestMove = { score: isMaximizing ? -Infinity : Infinity };
+
+	for (const slot of availableSlots) {
+		board[slot] = isMaximizing ? markRaw(IconMarkX) : markRaw(IconMarkO);
+
+		const { score } = minimax(board, !isMaximizing, alpha, beta);
+
+		board[slot] = null;
+
+		if (isMaximizing && score > bestMove.score) {
+			bestMove = { index: slot, score };
+			alpha = Math.max(alpha, score);
+		} else if (!isMaximizing && score < bestMove.score) {
+			bestMove = { index: slot, score };
+			beta = Math.min(beta, score);
+		}
+
+		if (alpha >= beta) break;
+	}
+
+	return bestMove;
+}
+
+function checkWinner(board, isMaximizing) {
+	const player = isMaximizing ? markRaw(IconMarkX) : markRaw(IconMarkO);
+	return winCombinations.value.some(([a, b, c]) => board[a] === player && board[b] === player && board[c] === player);
 }
 
 watchEffect(() => {
